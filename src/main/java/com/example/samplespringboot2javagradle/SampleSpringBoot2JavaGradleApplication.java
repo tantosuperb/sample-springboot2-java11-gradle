611fa26ba00a99fc581f97982a6c7a900040e1b7
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2023-09-13 17:46:43 +0900

    feat: 샘플프로젝트 생성

---
+++ src/main/java/com/example/samplespringboot2javagradle/SampleSpringBoot2JavaGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot2javagradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBoot2JavaGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringBoot2JavaGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplespringboot2javagradle/SampleSpringBoot2JavaGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot2javagradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBoot2JavaGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
