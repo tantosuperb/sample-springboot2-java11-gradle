3a2b0096d97be2ce099c5f7e968b1f143f264fea
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-02-14 13:51:28 +0900

    fix: Test 케이스 internal 프로파일로 변경
    외부 통신없이 성공해야하기 때문에 내부 통신(H2 InMemory) 로 변경함

--- src/test/java/com/example/samplespringboot2javagradle/SampleSpringBoot2JavaGradleApplicationTests.java
+++ src/test/java/com/example/samplespringboot2javagradle/SampleSpringBoot2JavaGradleApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.samplespringboot2javagradle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("internal")
 @SpringBootTest
 class SampleSpringBoot2JavaGradleApplicationTests {
 
